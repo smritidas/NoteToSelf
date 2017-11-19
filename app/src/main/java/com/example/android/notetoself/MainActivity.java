d87b490f0fce61c63f6a4f2269018586e666c31c
Smriti Das <dassmrit@gmail.com>
2017-11-19 15:09:49 -0500

    Implement noteadapter.

--- app/src/main/java/com/example/android/notetoself/MainActivity.java
+++ app/src/main/java/com/example/android/notetoself/MainActivity.java
@@ -8,7 +8,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,15 +26,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DialogShowNote dialog = new DialogShowNote();
-                dialog.sendNoteSelected(TempNote);
-                dialog.show(getFragmentManager(), "123");
-            }
-        });
+
+
+
+
+//        Button button = (Button) findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                DialogShowNote dialog = new DialogShowNote();
+//                dialog.sendNoteSelected(TempNote);
+//                dialog.show(getFragmentManager(), "123");
+//            }
+//        });
 
     }
 
@@ -50,5 +61,31 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    public class NoteAdapter extends BaseAdapter{
+
+        List<Note> noteList = new ArrayList<Note>();
+
+        @Override
+        public int getCount(){
+            return noteList.size();
+        }
+
+        @Override
+        public Note getItem(int whichItem){
+            return noteList.get(whichItem);
+        }
+
+        @Override
+        public long getItemId(int whichItem){
+            return whichItem;
+        }
+
+        @Override
+        public View getView(int whichItem, View view, ViewGroup viewGroup){
+            return view;
+        }
+
+    }
 
 }
